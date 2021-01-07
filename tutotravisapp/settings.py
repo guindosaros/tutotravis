86d7f1c90858bf9a8fcbaa364f70b6e843dc2dc6
guindosaros <mohamedsaros@gmail.com>
2021-01-07 11:19:54 +0000

    tuto package add

--- tutotravisapp/settings.py
+++ tutotravisapp/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    ############ Package 
+    # 'hvad',
+    
+    
+    ########
+    "testapp.apps.TestappConfig",
+    
 ]
 
 MIDDLEWARE = [
